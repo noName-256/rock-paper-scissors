ba7867d993eed014170961aa4cafb6111b1fbb24
noName-256 <altacc256@gmail.com>
2023-03-04 17:11:06 +0200

    Add GUI rounds, history functionality and refresh button functionality
    Format console game function, add gui rounds as well as a history for
    the rounds, link reset button to refresh function

--- javascript.js
+++ javascript.js
@@ -1,7 +1,9 @@
-let games = document.querySelector(".history");
-let rock= document.getElementById("rock");
-let paper= document.getElementById("paper");
-let scissors= document.getElementById("scissors");
+let rounds = document.querySelector(".history");
+let rock = document.getElementById("rock");
+let paper = document.getElementById("paper");
+let scissors = document.getElementById("scissors");
+let roundRes = document.querySelector(".result");
+let reset = document.querySelector(".reset-game-button");
 
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3) + 1;
@@ -41,29 +43,82 @@ function round(playerSelection, computerSelection) {
     }
 }
 
-function game()
-{
-    let playerScore=0, computerScore=0;
-    for(let i=0;i<5;i++)
-    {
-        let playerChoice, validChoice=false;
-        while(!validChoice)
-        {
-            playerChoice=prompt("Please enter your choice(rock, paper or scissors)", "");
-            playerChoice=playerChoice.toLowerCase();
-            if(playerChoice==="rock"||playerChoice==="paper"||playerChoice==="scissors")
-                validChoice=true;
-            if(!validChoice)console.log("Invalid input");
+function consoleGame() {
+    let playerScore = 0, computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        let playerChoice, validChoice = false;
+        while (!validChoice) {
+            playerChoice = prompt("Please enter your choice(rock, paper or scissors)", "");
+            playerChoice = playerChoice.toLowerCase();
+            if (playerChoice === "rock" || playerChoice === "paper" || playerChoice === "scissors")
+                validChoice = true;
+            if (!validChoice) console.log("Invalid input");
         }
-        let result=round(playerChoice, getComputerChoice());
+        let result = round(playerChoice, getComputerChoice());
         console.log(result);
-        if(result==="Draw!")playerScore++, computerScore++;
-        else if(result.charAt(4)==='w')playerScore++;
-        else if(result.charAt(4)==='l')computerScore++;
+        if (result === "Draw!") playerScore++, computerScore++;
+        else if (result.charAt(4) === 'w') playerScore++;
+        else if (result.charAt(4) === 'l') computerScore++;
         else throw new Error("Odd result/comparison");
     }
-    if(playerScore>computerScore)console.log("You win!");
-    else if(playerScore===computerScore)console.log("It's a draw, a sensation!");
+    if (playerScore > computerScore) console.log("You win!");
+    else if (playerScore === computerScore) console.log("It's a draw, a sensation!");
     else console.log("You lose...");
 }
 /*You can call game to play the game in the console, but I will use a GUI for the website*/
+
+function GUIgame() {
+let playerScore = 0, computerScore = 0;
+function addRoundToHistory(playerChoice, computerChoice, roundResult) {
+    let li = document.createElement("li");
+    roundResult = roundResult.toLowerCase();
+    switch (roundResult) {
+        case "win":
+            li.style.backgroundColor = "green";
+            roundRes.textContent="Win!";
+            roundRes.style.color = "green";
+            playerScore++;
+            break;
+        case "lose":
+            li.style.backgroundColor = "red";
+            roundRes.textContent="Loss.";
+            roundRes.style.color = "red";
+            computerScore++;
+            break;
+        case "draw":
+            li.style.backgroundColor = "yellow";
+            roundRes.textContent="Draw";
+            roundRes.style.color = "yellow";
+            playerScore++;
+            computerScore++;
+            break;
+        default:
+            throw new Error("Odd roundResult/comparison");
+    }
+    rounds.appendChild(li);
+    let div1 = document.createElement("div");
+    div1.textContent = playerChoice;
+    let div2 = document.createElement("div");
+    div2.textContent = (roundResult === "draw" ? "=" : (roundResult === "win" ? ">" : "<"));
+    let div3 = document.createElement("div");
+    div3.textContent = computerChoice;
+    li.appendChild(div1);
+    li.appendChild(div2);
+    li.appendChild(div3);
+
+}
+function playRound(playerChoice) {
+    let computerChoice = getComputerChoice();
+    computerChoice = computerChoice.toLowerCase();
+    let result = round(playerChoice, computerChoice);
+    if (result === "Draw!") playerScore++, computerScore++, addRoundToHistory(playerChoice, computerChoice, "draw");
+    else if (result.charAt(4) === 'w') playerScore++, addRoundToHistory(playerChoice, computerChoice, "win");
+    else if (result.charAt(4) === 'l') computerScore++, addRoundToHistory(playerChoice, computerChoice, "lose");
+    else throw new Error("Odd result/comparison");
+}
+rock.addEventListener("click", () => { playRound("rock"); });
+paper.addEventListener("click", () => { playRound("paper"); });
+scissors.addEventListener("click", () => { playRound("scissors"); });
+reset.addEventListener("click", ()=>{document.location.reload();});
+}
+GUIgame();
